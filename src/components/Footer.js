c2919d9a7d7de627d8de76db17bdca3fb132077c
abissier <71886314+abissier@users.noreply.github.com>
2021-03-24 11:43:48 -0700

    added footer

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+function Footer() {
+	return (
+		<div className="footer">
+			<a href="https://github.com/abissier" target="_blank" rel="noreferrer">
+				<img src="https://img.icons8.com/material-sharp/24/000000/github.png" alt="github" />
+			</a>
+			<a href="mailto:abissieregrote@gmail.com">
+				<img src="https://img.icons8.com/fluent-systems-filled/24/000000/email-sign.png" alt="email" />
+			</a>
+			<a href="./img/ABG Tech Resume .pdf" download="ABGResume">
+				<img src="https://img.icons8.com/material/24/000000/set-as-resume--v1.png" alt="resume" />
+			</a>
+		</div>
+	);
+}
+
+export default Footer;
